6288afc05f7a9d0f85fa8038c36a0423b5dc9cd2
Cleber Junio <juninhosete@yahoo.com.br>
2024-09-06 16:12:07 -0300

    feat: add component Categories and style of categories

---
+++ src/components/categories/categories.components.tsx
@@ -0,0 +1,13 @@
+import './categories.styles.css'
+
+const Categories = () =>{
+  return(
+    <div className="categories-container">
+      <div className="categories-content">
+
+      </div>
+    </div>
+  )
+}
+
+export default Categories
